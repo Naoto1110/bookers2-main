001f8d127623c0b24affa9b3b94145eb6dcfa277
EC2 Default User <ec2-user@ip-172-31-28-17.ap-northeast-1.compute.internal>
2023-07-18 01:33:23 +0000

    favorit-Finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,58 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @user = User.find(current_user.id)
+    @books = Book.all
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      render 'index'
+    end
+  end
+
+  def show
+    @new_book =Book.new
+    @book = Book.find(params[:id])
+    @user =@book.user
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book), notice: "You have updated book successfully."
+   else
+      render "edit"
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def edit
+    edit_user
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+  def edit_user
+    book = Book.find(params[:id])
+    unless book.user == current_user
+      redirect_to books_path
+    end
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  resources :books, only: [:edit, :index, :show, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources:book_comments,only: [:create,:destroy]
+  end
+  resources :users, only: [:edit, :index, :show, :update]
+  get "/home/about" => "homes#about", as: "about"
+end
